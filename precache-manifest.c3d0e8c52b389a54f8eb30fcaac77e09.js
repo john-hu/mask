f8f475b58f046867c4fbe109bcf47438c05d6bae
John Hu <im@john.hu>
2022-02-10 16:58:46 +0800

    Updates

---
+++ precache-manifest.c3d0e8c52b389a54f8eb30fcaac77e09.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d58bf14b60e9beb9ee08",
+    "url": "/mask/static/css/main.adee548a.chunk.css"
+  },
+  {
+    "revision": "d58bf14b60e9beb9ee08",
+    "url": "/mask/static/js/main.d58bf14b.chunk.js"
+  },
+  {
+    "revision": "082839daa0c70a90be63",
+    "url": "/mask/static/js/1.082839da.chunk.js"
+  },
+  {
+    "revision": "48c84af8f537debcf5ba",
+    "url": "/mask/static/js/runtime~main.48c84af8.js"
+  },
+  {
+    "revision": "7235dab0c5981d5e45e7ded8168289d7",
+    "url": "/mask/index.html"
+  }
+];
